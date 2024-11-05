5f4032c8b531074f97bcc8941dbde193ba296e0e
koalla05 <kov.koalla@gmail.com>
2024-11-05 19:05:44 +0200

    Pipes without vectors

--- PipePool.cpp
+++ PipePool.cpp
@@ -3,79 +3,56 @@
 #include <iostream>
 
 PipePool::PipePool() {
-    pool1.emplace_back(600, "green", false);
-    pool2.emplace_back(600, "green", true);
-    pool3.emplace_back(600, "yellow", false);
-    pool4.emplace_back(600, "yellow", true);
-
     // pool1.emplace_back(600, "green", false);
-    // pool1.emplace_back(600, "green", true);
-    // pool1.emplace_back(600, "green", false);
-    // pool1.emplace_back(600, "green", true);
-    // pool1.emplace_back(600, "green", false);
-    // // pool1.emplace_back(600, "green", true);
-    // // pool1.emplace_back(600, "green", false);
-    // // pool1.emplace_back(600, "green", true);
-    //
-    // pool2.emplace_back(600, "yellow", false);
-    // pool2.emplace_back(600, "yellow", true);
-    // // pool2.emplace_back(600, "yellow", false);
-    // // pool2.emplace_back(600, "yellow", true);
-    //
-    // pool3.emplace_back(600, "green", false);
-    // pool3.emplace_back(600, "green", true);
+    // pool2.emplace_back(600, "green", true);
     // pool3.emplace_back(600, "yellow", false);
-    // pool3.emplace_back(600, "yellow", true);
+    // pool4.emplace_back(600, "yellow", true);
+    pipe1 = new Pipe(600, "green", false);
+    pipe2 = new Pipe(600, "green", true);
+    pipe3 = new Pipe(600, "yellow", false);
+    pipe4 = new Pipe(600, "yellow", true);
 
 }
 
 Pipe& PipePool::getPipe() {
-    if (diff == 1) {
-        if (currentIndex == 0) {
-            currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool3[0];
-            return pipe;
-        }
-        else if (currentIndex == 1) {
-            currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool4[0];
-            return pipe;
-        }
-    }
-    else if (diff == 2) {
-        if (currentIndex == 0) {
-            currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool1[0];
-            return pipe;
-        }
-        else if (currentIndex == 1) {
-            currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool2[0];
-            return pipe;
-        }
-    }
+    switch (diff) {
+        case 1:
+            switch (currentIndex) {
+                case 0:
+                    currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separately)
+                return (*pipe3);
+                case 1:
+                    currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separately)
+                return (*pipe4);
+            }
+        break;
 
-    else if (diff == 3) {
-        if (currentIndex == 0) {
-            currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool1[0];
-            return pipe;
-        }
-        else if (currentIndex == 1) {
-            currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool2[0];
-            return pipe;
-        }
-        else if (currentIndex == 2) {
-            currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool3[0];
-            return pipe;
-        }
-        else {
-            currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separetely)
-            Pipe& pipe = pool4[0];
-            return pipe;
-        }
+        case 2:
+            switch (currentIndex) {
+                case 0:
+                    currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separately)
+                return (*pipe1);
+                case 1:
+                    currentIndex = (currentIndex + 1) % 2; // number of different pipes (top and bottom separately)
+                return (*pipe2);
+            }
+        break;
+
+        case 3:
+            switch (currentIndex) {
+                case 0:
+                    currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separately)
+                return (*pipe1);
+                case 1:
+                    currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separately)
+                return (*pipe2);
+                case 2:
+                    currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separately)
+                return (*pipe3);
+                case 3:
+                    currentIndex = (currentIndex + 1) % 4; // number of different pipes (top and bottom separately)
+                return (*pipe4);
+            }
     }
 
 }
--- PipePool.h
+++ PipePool.h
@@ -17,5 +17,10 @@ private:
     std::vector<Pipe> pool2;
     std::vector<Pipe> pool3;
     std::vector<Pipe> pool4;
+
+    Pipe* pipe1;
+    Pipe* pipe2;
+    Pipe* pipe3;
+    Pipe* pipe4;
     size_t currentIndex = 0;
 };
